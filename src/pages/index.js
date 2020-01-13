130021bd0b8f83fa96410f36dcabc4f4d4b516f7
emontes <emontes@turista.com.mx>
2020-01-13 15:13:44 -0500

    primer commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import SimleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
+export default () => (
+  <Layout>
+    <SimleHero>
+      <Banner
+        title="continua explorando"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimleHero>
+    <About />
+    <Services />
+  </Layout>
+)
