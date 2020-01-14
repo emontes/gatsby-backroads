ad3fdad079df7d394bf9fdc86e97e3bd4afa6dc9
emontes <emontes@turista.com.mx>
2020-01-14 08:52:09 -0500

    Forma de contacto codigo hasta 140 FormSpree Service

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -2,14 +2,11 @@ import React, { Component } from "react"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
-
-import Images from '../examples/Images';
 export default class tours extends Component {
   render() {
     return (
       <Layout>
         <StyledHero img={this.props.data.defaultBcg.childImageSharp.fluid} />
-        <Images />
       </Layout>
     )
   }
